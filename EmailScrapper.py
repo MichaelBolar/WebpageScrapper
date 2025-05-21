d5526b75e25705be29429dbb58547ba0d2c9b976
MichaelBolar <94660242+MichaelBolar@users.noreply.github.com>
2025-05-20 19:10:57 -0600

    Add files via upload

---
+++ EmailScrapper.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+
+setdirc = set()
+dotset = set()
+
+browser = webdriver.Firefox()
+
+browser.get('https://goatlantalocal.com')
+
+elem = browser.find_element('link text', 'SEARCH LOCAL BUSINESSES')
+
+elem.click()
+
+elems = browser.find_elements('tag name', 'a')
+
+#For each anchor tag found, filter out the links with href
+#and save them to the variable links
+for elem in elems:
+    links = elem.get_attribute('href')
+    
+    #Filters the links we really want to access and saves them to a unordered set
+    if 'businesses/atlanta' in links:
+        setdirc.add(links)
+
+#Go through each set element, and open each companies page
+for i in setdirc:
+    browser.get(i)
+    webs = browser.find_elements('tag name', 'a')
+
+    #Look for the href attribute on each companies page
+    #If the link is between 30 and 37 characters, add it to a set and print the set
+    for web in webs:
+        dotcom = web.get_attribute('href')
+        if dotcom == None:
+            continue
+        if len(dotcom) in range(30, 37):
+            dotset.add(dotcom)
+print(dotset)
+
+#Open up each companies website
+for j in dotset:
+    browser.get(j)
+
+#If the company has an email address, find it and save it somewhere
+ 
+
+    
